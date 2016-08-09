11ab5cf44def4cd17deb54c8af26b4981a3548e2
SIH-HUA <wusihhua0323@gmail.com>
2016-08-09 10:23:28 +0800

    add button

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView); //須強制轉型，因型態不同
+
+    }
+
+    public void click(View view) //因為view是所有原件的parent，因此用view
+    {
+        textView.setText("Hello textView!");
     }
 }
