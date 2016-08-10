d69ac5696fa81b1dcbbdb56110099aad5aff956f
SIH-HUA <wusihhua0323@gmail.com>
2016-08-10 09:54:18 +0800

    add listView

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ListView listView;
+
     String drink = "Black Tea"; //初始飲料
 
     @Override
@@ -33,12 +37,21 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        listView = (ListView)findViewById(R.id.listView);
+        setListView();
     }
-            public void click(View view) //因為view是所有原件的parent，因此用view
-            {
-                String text = editText.getText().toString(); //將轉成字串，去除其他屬性
-                text = text + " Order: " + drink;
-                textView.setText(text);
-                editText.setText(" ");
-            }
+    private void setListView()
+    {
+        String[] data = new String[]{"1","2","3","4","5","6","7","8"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,data); //轉換成每筆有用的ITEM
+        listView.setAdapter(adapter);
+    }
+    public void click(View view) //因為view是所有原件的parent，因此用view
+    {
+        String text = editText.getText().toString(); //將轉成字串，去除其他屬性
+        text = text + " Order: " + drink;
+        textView.setText(text);
+        editText.setText(" ");
+    }
         }
