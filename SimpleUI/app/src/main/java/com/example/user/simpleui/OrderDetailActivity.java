eb0dce54ec32eaf5f09465f1bb56c976f69deb15
SIH-HUA <wusihhua0323@gmail.com>
2016-08-18 12:12:45 +0800

    thread and hanlder

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.user.simpleui;
 
 import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import android.os.Handler;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -15,7 +18,7 @@ public class OrderDetailActivity extends AppCompatActivity {
         TextView noteTxetView = (TextView)findViewById(R.id.noteTextView);
         TextView storeInfoTxetView = (TextView)findViewById(R.id.storeInfoTextView);
         TextView drinkOrderResultsTxetView = (TextView)findViewById(R.id.drinkOrderResultsTextView);
-        TextView latlngTextView = (TextView)findViewById(R.id.latlngTextView);
+        final TextView latlngTextView = (TextView)findViewById(R.id.latlngTextView);
 
         Intent intent = getIntent();
         Order order = intent.getParcelableExtra("order");
@@ -30,5 +33,30 @@ public class OrderDetailActivity extends AppCompatActivity {
             resultText +=drinkName+" M:"+mNumber+" L:"+lNumber+"\n";
         }
         drinkOrderResultsTxetView.setText(resultText);
+
+        final Handler handler = new Handler(new Handler.Callback() //當有人傳訊息給handler，handler會叫mainthread去做事情
+        {
+            @Override
+            public boolean handleMessage(Message msg) {
+                latlngTextView.setText("123456"); //更改UI的事情只能用handler叫mainthread做，不能直接放到thread\做
+                return false;
+            }
+        }); //屬於android
+
+        Thread thread = new Thread(new Runnable()//Runnable表示一件事情，一個Thread可以做很多件事情
+        {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(10000); // 10秒
+                    handler.sendMessage(new Message());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        //thread.run(); // 會先等MainThread作，在做我們的thread會延遲10秒，會變黑10秒
+        thread.start(); //在background做，因此畫面不會變黑
+
     }
 }
